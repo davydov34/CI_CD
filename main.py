ee4d3b2a84918bfa27c64e916def23f2a982775a
Davydov Kirill <davydov34@gmai.com>
2025-04-26 07:12:58 +0300

    2st

---
+++ main.py
@@ -0,0 +1,8 @@
+from calc import add, sub
+
+if __name__ == "__main__":
+    one = add(3, 3)
+    two = sub(10, 5)
+    three = add(199, 1)
+    four = sub(200, 1)
+    print(f'Res: \n{one}\n{two}\n{three}\n{four}')
---
+++ test_calc.py
@@ -0,0 +1,9 @@
+import calc
+
+
+def test_add():
+    assert calc.add(3, 1) == 4
+
+
+def test_sub():
+    assert calc.sub(3, 1) == 2
